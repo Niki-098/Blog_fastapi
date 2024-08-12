add5896a2fe1540a093b01300a5987299eab443d
Nikita <kniki098@gmail.com>
2024-08-12 19:45:19 +0530

    Query parameter and request body

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog list'}
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
+    # only get 10 published blogs
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
 
 
 @app.get('/blog/unpublished')
@@ -21,8 +27,18 @@ def show(id: int):
 
 
 
-
 @app.get('/blog/{id}/comments')
 def comments(id):
     # fetch comments of blog with id = id
     return {'data':{'1','2'}}
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published_at:Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
